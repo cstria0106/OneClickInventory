e2df9668951127741467a9ac2725278c2a3bed86
cstria0106 <cstria0106@gmail.com>
2024-03-03 01:11:49 +0900

    Revert "remove weird import"
    This reverts commit bb56b73be21cff579f938c5fbdebe07f1f492822.

--- Editor/Generator/AnimationGenerator.cs
+++ Editor/Generator/AnimationGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using dog.miruku.inventory.runtime;
+using PlasticGui.Configuration.OAuth;
 using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
