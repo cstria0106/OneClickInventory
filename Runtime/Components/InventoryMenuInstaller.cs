462d46d0aae6f6fac3ce9cee4620427bd964f9b7
cstria0106 <cstria0106@gmail.com>
2024-05-08 23:28:33 +0900

    add IEditorOnly to InventoryMenuInstaller

--- Runtime/Components/InventoryMenuInstaller.cs
+++ Runtime/Components/InventoryMenuInstaller.cs
@@ -1,11 +1,12 @@
 
 using UnityEngine;
+using VRC.SDKBase;
 
 namespace dog.miruku.inventory.runtime
 {
     [DisallowMultipleComponent]
     [AddComponentMenu("One-Click Inventory/Inventory Menu Installer")]
-    public class InventoryMenuInstaller : MonoBehaviour
+    public class InventoryMenuInstaller : MonoBehaviour, IEditorOnly
     {
         [SerializeField] private Inventory _inventory;
         public Inventory Inventory { get => _inventory; set => _inventory = value; }
